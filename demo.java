d41e5e6def8b0806821d8c0418d1215bc591d3fd
Skyjohnkiran <152399810+Skyjohnkiran@users.noreply.github.com>
2023-12-02 00:18:24 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,6 +2,7 @@ public class demo{
  public static void main(String[] args){
   String Name="johnkiran";
   String address="gudipadu";
+  Long MobileNum=7901457967L;
   int age=23;
   System.out.println("******************");
   System.out.println("Name:"+Name);
